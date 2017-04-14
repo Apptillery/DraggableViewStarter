da0638d92b6449e9d1744caaf4386efa244ec6cd
Alex Gibson <oakmonttech@gmail.com>
2017-04-14 09:02:07 -0400

    DraggableView Starter
    A starter project for a DraggableView tutorial on apptillery.info

---
+++ Draggable/DraggableView.swift
@@ -0,0 +1,18 @@
+//
+//  Draggable.swift
+//  Draggable
+//
+//  Created by Apptillery on 4/12/17.
+//  Copyright © 2017 Apptillery. All rights reserved.
+//
+
+import UIKit
+
+protocol DraggableViewDelegate {
+    
+}
+
+class DraggableView: UIView {
+
+
+}
---
+++ Draggable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Draggable
+//
+//  Created by Apptillery on 4/12/17.
+//  Copyright © 2017 Apptillery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
